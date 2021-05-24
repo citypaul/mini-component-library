34f9587818d4426ccf07762b698b2d05f199d9d6
Paul Hammond <paul.hammond@gmail.com>
2021-05-24 17:12:03 +0100

    started styling

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,16 +6,29 @@ import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
 const Wrapper = styled.div``;
+const StyledBar = styled.div`
+  background-color: ${COLORS.primary};
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  border-top-right-radius: ${(props) => (props.value === 100 ? "4px" : "none")};
+  border-bottom-right-radius: ${(props) =>
+    props.value === 100 ? "4px" : "none"};
+  height: 12px;
+  width: ${(props) => props.value}%;
+`;
 
 const ProgressBar = ({ min = 0, max = 100, value = 0, size }) => {
   return (
-    <Wrapper
-      aria-valuenow={value}
-      aria-valuemin={min}
-      aria-valuemax={max}
-      role="progressbar"
-    >
-      Hello
+    <Wrapper>
+      <StyledBar
+        aria-valuenow={value}
+        aria-valuemin={min}
+        aria-valuemax={max}
+        min={min}
+        max={max}
+        value={value}
+        role="progressbar"
+      ></StyledBar>
     </Wrapper>
   );
 };
