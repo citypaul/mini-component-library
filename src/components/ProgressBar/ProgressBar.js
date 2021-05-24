8280cc99eae447be0e05a6f1db3dea2d25b5be97
Paul Hammond <paul.hammond@gmail.com>
2021-05-24 16:42:38 +0100

    first couple of tests passing

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,23 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const Wrapper = styled.div``;
+
+const ProgressBar = ({ min = 0, max = 100, value = 0, size }) => {
+  return (
+    <Wrapper
+      aria-valuenow={value}
+      aria-valuemin={min}
+      aria-valuemax={max}
+      role="progressbar"
+    >
+      Hello
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
--- src/components/ProgressBar/ProgressBar.test.jsx
+++ src/components/ProgressBar/ProgressBar.test.jsx
@@ -2,6 +2,48 @@ import React from "react";
 import ProgressBar from "./ProgressBar";
 import { render, screen } from "@testing-library/react";
 
-test("Renders", () => {
-  const component = render(<ProgressBar />);
+test("Defaults to a min of zero and max of 100", async () => {
+  render(<ProgressBar />);
+
+  const component = await screen.findByRole("progressbar");
+
+  expect(component).toHaveAttribute("aria-valuenow", "0");
+  expect(component).toHaveAttribute("aria-valuemin", "0");
+  expect(component).toHaveAttribute("aria-valuemax", "100");
 });
+
+test("Can customise the min, max and current values", async () => {
+  const expectedMinValue = 50;
+  const expectedMaxValue = 150;
+  const expectedNowValue = 85;
+
+  render(
+    <ProgressBar
+      min={expectedMinValue}
+      max={expectedMaxValue}
+      value={expectedNowValue}
+    />
+  );
+
+  const component = await screen.findByRole("progressbar");
+
+  expect(component).toHaveAttribute(
+    "aria-valuenow",
+    expectedNowValue.toString()
+  );
+
+  expect(component).toHaveAttribute(
+    "aria-valuemin",
+    expectedMinValue.toString()
+  );
+
+  expect(component).toHaveAttribute(
+    "aria-valuemax",
+    expectedMaxValue.toString()
+  );
+});
+
+test.todo("Can increment the value");
+test.todo(
+  "Will set to default max value if value now is initialised higher than the default max value"
+);
