6d174bf02faf14b5afc09ebef16789f8135f34de
Paul Hammond <paul.hammond@gmail.com>
2021-05-24 20:24:28 +0100

    minor refactoring

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -13,16 +13,12 @@ const StyledBar = styled.div`
   background-color: ${COLORS.primary};
   border-top-left-radius: 4px;
   border-bottom-left-radius: 4px;
-  border-top-right-radius: ${(props) =>
-    calculatePercentage(props.value, props.min, props.max) === 100
-      ? "4px"
-      : "none"};
-  border-bottom-right-radius: ${(props) =>
-    calculatePercentage(props.value, props.min, props.max) === 100
-      ? "4px"
-      : "none"};
+  border-top-right-radius: ${({ value, min, max }) =>
+    calculatePercentage(value, min, max) === 100 ? "4px" : "none"};
+  border-bottom-right-radius: ${({ value, min, max }) =>
+    calculatePercentage(value, min, max) === 100 ? "4px" : "none"};
   height: 12px;
-  width: ${(props) => calculatePercentage(props.value, props.min, props.max)}%;
+  width: ${({ value, min, max }) => calculatePercentage(value, min, max)}%;
 `;
 
 const ProgressBar = ({ min = 0, max = 100, value = 0, size }) => {
